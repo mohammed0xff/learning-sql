64484b8bc2c697e24d8b8c5be4e308eb86a8018c
mohammed0xff <81110384+mohammed0xff@users.noreply.github.com>
2022-02-08 23:46:03 +0200

    Create 11.sql

---
+++ 11.sql
@@ -0,0 +1,5 @@
+	--Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
+  
+  
+	SELECT il.InvoiceId, COUNT(*) FROM InvoiceLine AS il
+	GROUP BY il.InvoiceId;
