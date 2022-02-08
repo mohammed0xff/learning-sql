77982ac738edbee9c4e9a2e61a95f70878a6fd67
mohammed0xff <81110384+mohammed0xff@users.noreply.github.com>
2022-02-08 23:54:38 +0200

    Create 14.sql

---
+++ 14.sql
@@ -0,0 +1,5 @@
+	--Provide a query that shows the # of invoices per country. HINT: GROUP BY
+
+	SELECT c.Country, COUNT(*) AS 'Total Invoices' FROM Invoice AS i
+	LEFT OUTER JOIN Customer AS c ON (c.CustomerId = i.CustomerId)
+	GROUP BY c.Country;
